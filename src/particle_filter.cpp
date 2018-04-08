5dc6bd3885ac844b2d58a77a4850701d90fdae60
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-04-07 17:01:34 -0700

    add prediction routine

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -57,6 +57,34 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
+	
+	
+	default_random_engine gen;
+	
+	for (int i = 0; i < num_particles; i++)
+	{
+		double new_x;
+		double new_y;
+		double new_theta;
+		
+		if (yaw_rate == 0) {
+			new_x = particles[i].x + velocity*delta_t*cos(particles[i].theta);
+			new_x = particles[i].y + velocity*delta_t*sin(particles[i].theta);
+			new_theta = particles[i].theta;
+		} else{
+			new_x = particles[i].x + velocity/yaw_rate*(sin(particles[i].theta + yaw_rate*delta_t) - sin(particles[i].theta));
+			new_y = particles[i].x + velocity/yaw_rate*(cos(particles[i].theta) - cos(particles[i].theta + yaw_rate*delta_t));
+			new_theta = particles[i].theta + yaw_rate*delta_t;
+		}
+		
+		normal_distribution<double> N_x(new_x, std_pos[0]);
+	    normal_distribution<double> N_y(new_y, std_pos[1]);
+	    normal_distribution<double> N_theta(new_theta, std_pos[2]);
+		
+		particles[i].x = N_x(gen);
+		particles[i].y = N_y(gen);
+		particles[i].theta = N_theta(gen);
+	}
 
 }
 
